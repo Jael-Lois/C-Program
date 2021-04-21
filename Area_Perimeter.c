a6add379f8b3ac4c45a99162b933b46eb7621998
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-04-21 19:38:09 +0530

    Update Area_Perimeter.c

--- Area_Perimeter.c
+++ Area_Perimeter.c
@@ -1,29 +1,42 @@
-// Online C compiler to run C program online
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int ch;
+    int ch,s1,l,b;
+    begin: 
     printf("***************************************************************************");
     printf("\nsquare         : 1");
-    printf("\nrectangle      : 2\n");
-    printf("***************************************************************************");
+    printf("\nrectangle      : 2");
+    printf("\nexit           : 3\n");
+    printf("***************************************************************************\n");
     printf("\n enter your choice");
     scanf("%d", &ch);
+    printf("***************************************************************************\n");
+    
     switch(ch)
     {
-        case 1: int s1;
-                printf("enter the side of the squre :");
+        case 1: 
+                // int s1;
+                printf("enter the side of the square :");
                 scanf("%d",&s1);
                 printf("\n area = %d",s1*s1);
-                printf("\n perimeter = %d", 4*s1);
+                printf("\n perimeter = %d \n", 4*s1);
                 break;
-        case 2: int l,b;
+        case 2: 
+               // int l,b;
                 printf("\n enter the length and bredth of a rectangle :");
                 scanf("%d",&l);
                 scanf("%d",&b);
                 printf("\n area = %d ",l*b);
-                printf("")
-    }
+                printf("\n perimeter = %d \n",2*(l+b));
+               break;
+        case 3: 
+                break;
+        default:
+               printf("\n please enter values 1 & 2");
+    } 
+    if (ch <=2)
+        goto begin;
+        
     
     return 0;
 }
