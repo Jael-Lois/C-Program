e4bfb4bd84ec810cb36592212c6a43d741960487
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-04-23 16:04:38 +0530

    Update Quadratic_Equation.c

--- Quadratic_Equation.c
+++ Quadratic_Equation.c
@@ -1,39 +1,59 @@
 #include<stdio.h>
+
 #include<math.h>
 
-  int main()
+int main()
+
 {
-        float a,b,c;
 
-        printf("Enter the value for a : ");
+  float a,b,c;
 
-        scanf("%f",&a);
+  printf("Enter the value for a : ");
 
-        printf("\nEnter the value for b : ");
+  scanf("%f",&a);
 
-        scanf("%f",&b);
+  printf("\nEnter the value for b : ");
 
-        printf("\nEnter the value for c : ");
+  scanf("%f",&b);
 
-        scanf("%f",&c);
+  printf("\nEnter the value for c : ");
 
-        if ((b*b)-(4*a*c)==0)
-        {
-          printf("the roots are %.2f",-b/(2*a));
-          printf(" the roots are equal");
-        }
-        else if ((b*b)-(4*a*c)<0)
-        {
-           printf("The value of x is %.2f + %.2fi",(-b/(2*a)),sqrt((4*a*c)-b*b));
-           printf("\n\nThe value of x is %.2f - %.2fi",(-b/(2*a)),sqrt((4*a*c)-b*b));
-           printf("\n\nThe roots are imaginary.");
-         }
-        else
-        {
-           printf("The value of x is %.2f",(-b+sqrt(b*b-4*a*c))/2*a);
-           printf("\n\nThe value of x is %.2f",(-b-sqrt(b*b-4*a*c))/2*a);
-           printf("\n\nThe roots are distinct.");
-         }
-      return 0;
+  scanf("%f",&c);
+
+  if ((b*b)-(4*a*c)==0)
+
+  {
+
+    printf("\nThe root is equal");
+
+    printf("\nThe value of the root is %.2f",-b/(2*a));
+
+  }
+
+  else if ((b*b)-(4*a*c)<0)
+
+  {
+
+    printf("\nThe root is imaginary.");
+
+    printf("\nThe value of the root are %.2f + %.2f i",-b/(2*a),sqrt(4*a*c-b*b)/(2*a));
+
+    printf("\nThe value of the root are %.2f - %.2f i",-b/(2*a),sqrt(4*a*c-b*b)/(2*a));
+
+  }
+
+  else
+
+  {
+
+    printf("\nThe roots are distinct.");
+
+    printf("\nThe value of the root is %.2f",(-b + sqrt((b*b)-(4*a*c)))/(2*a));
+
+    printf("\nThe value of the root is %.2f",(-b - sqrt((b*b)-(4*a*c)))/(2*a));
+
+  }
+
+  return 0;
 
 }
