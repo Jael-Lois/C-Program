931876c29037c885193456a85ce668e1b3e3270f
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-04-23 14:46:08 +0530

    Update Quadratic_Equation.c

--- Quadratic_Equation.c
+++ Quadratic_Equation.c
@@ -3,35 +3,35 @@
 
   int main()
 {
-        int a,b,c;
+        float a,b,c;
 
         printf("Enter the value for a : ");
 
-        scanf("%d",&a);
+        scanf("%f",&a);
 
         printf("\nEnter the value for b : ");
 
-        scanf("%d",&b);
+        scanf("%f",&b);
 
         printf("\nEnter the value for c : ");
 
-        scanf("%d",&c);
+        scanf("%f",&c);
 
         if ((b*b)-(4*a*c)==0)
         {
-          printf("the roots are %d",-b/(2*a));
+          printf("the roots are %.2f",-b/(2*a));
           printf(" the roots are equal");
         }
         else if ((b*b)-(4*a*c)<0)
         {
-           printf("The value of x is %d + %di",(-b/(2*a)),sqrt((4*a*c)-b*b));
-           printf("\n\nThe value of x is %d - %di",(-b/(2*a)),sqrt((4*a*c)-b*b));
+           printf("The value of x is %.2f + %.2fi",(-b/(2*a)),sqrt((4*a*c)-b*b));
+           printf("\n\nThe value of x is %.2f - %.2fi",(-b/(2*a)),sqrt((4*a*c)-b*b));
            printf("\n\nThe roots are imaginary.");
          }
         else
         {
-           printf("The value of x is %d",(-b+sqrt(b*b-4*a*c))/2*a);
-           printf("\n\nThe value of x is %d",(-b-sqrt(b*b-4*a*c))/2*a);
+           printf("The value of x is %.2f",(-b+sqrt(b*b-4*a*c))/2*a);
+           printf("\n\nThe value of x is %.2f",(-b-sqrt(b*b-4*a*c))/2*a);
            printf("\n\nThe roots are distinct.");
          }
       return 0;
