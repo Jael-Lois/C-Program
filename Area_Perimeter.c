a633acaa73a047aea5a96ae577086eb2d1b5fcd7
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-04-25 11:19:25 +0530

    Update Area_Perimeter.c

--- Area_Perimeter.c
+++ Area_Perimeter.c
@@ -1,6 +1,9 @@
-int main() {
+#include<stdio.h>
+int main()
+{
     int ch,s1,s2,s3,s4,d,h;
-    begin: 
+    float d1;
+    begin:
     printf("\n*************************************");
     printf("\nSquare              : 1");
     printf("\nRectangle           : 2");
@@ -8,34 +11,35 @@ int main() {
     printf("\nQuadrilateral       : 4");
     printf("\nParallelogram       : 5");
     printf("\nTrapezium           : 6");
-    printf("\nExit                : 7\n");
+    printf("\nRhombus            : 7");
+    printf("\nExit                : 8\n");
     printf("\n***********************************\n");
     printf("\n Enter your choice");
     scanf("%d", &ch);
     printf("\n***********************************************************\n");
-    
+
     switch(ch)
     {
-        case 1: 
+        case 1:
                 printf("Enter the side of the square :");
                 scanf("%d",&s1);
                 printf("\n Area = %d",s1*s1);
                 printf("\n Perimeter = %d \n", 4*s1);
                 break;
-        case 2: 
+        case 2:
                 printf("\n Enter the length and bredth of a rectangle :");
                 scanf("%d",&s1);
                 scanf("%d",&s2);
                 printf("\n Area = %d ",s1*s2);
                 printf("\n Perimeter = %d \n",2*(s1+s2));
                break;
-        case 3: 
+        case 3:
                 printf("\nEnter the side of the equilateral triangle :");
                 scanf("%d",&s1);
                 printf("\n Area = %f",(1.732/4)*s1*s1);
                 printf("\n Primeter = %d \n",3*s1);
                 break;
-                
+
         case 4:
                 printf("Enter the sides of the quadrilateral : \n");
                 printf("Enter the diagonal of the quadrilateral : \n");
@@ -47,7 +51,7 @@ int main() {
                 printf("\n Area = %f \n",1/2.0*d*s1*s2);
                 printf("\n Primeter = %d \n",s1+s2+s3+s4);
                 break;
-        
+
         case 5:
                 printf("Enter the base of the parallelogram : \n");
                 printf("Enter the height of the parallelogram : \n");
@@ -56,8 +60,8 @@ int main() {
                 printf("\nArea = %d \n",s1*s2);
                 printf("\nPerimeter = %d \n",2*(s1+s2));
                 break;
-                
-                
+
+
         case 6:
                 printf("Enter the height of the trapezium : \n");
                 scanf("%d",&h);
@@ -70,17 +74,26 @@ int main() {
                 printf("\n Area = %f",1/2.0*h*(s2+s3));
                 printf("\n Perimeter = %d",s1+s2+s3+s4);
                 break;
-                
+
         case 7:
+                printf("Enter the diagonals for the rhombus : \n");
+                printf("Enter the side of the rhombus : \n");
+                scanf("%d %f",&d,&d1);
+                scanf("%d",&s1);
+                printf("\n Area = %f",1/2.0*d*d1);
+                printf("\n Perimeter = %d",4*s1);
                 break;
-                
-                
+        case 8:
+                break;
+
+
+
         default:
-               printf("\n please enter values 1 & 7 \n");
-    } 
-    if (ch !=7)
+               printf("\n please enter values 1 & 8 \n");
+    }
+    if (ch !=8)
         goto begin;
-        
-    
+
+
     return 0;
 }
