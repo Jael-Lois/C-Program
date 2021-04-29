73b4bfb543dc326ade5918b9942db2be9adba3ec
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-04-29 10:36:59 +0530

    Create divisible_by_5_and_11.c

---
+++ divisible_by_5_and_11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+
+    //divisible by 5 and 11
+
+    int y,a,b;
+
+    printf("Enter the number to check whther it is divisible by 5 and 11 :");
+
+    scanf("%d",&y);
+
+    a = y%5;
+
+    b = y%11;
