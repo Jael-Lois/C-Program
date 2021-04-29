0fdf5b09a7497256119431213974dffc2fbdc9ee
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-04-29 10:37:22 +0530

    Update divisible_by_5_and_11.c

--- divisible_by_5_and_11.c
+++ divisible_by_5_and_11.c
@@ -14,3 +14,14 @@ int main()
     a = y%5;
 
     b = y%11;
+
+    if((a+b)==0)
+
+    printf("\nYour number is divisible by 5 and 11 ");
+
+    else
+
+    printf("\nThe number you have entered is not divisible by 5 or 11 or both");
+
+    return 0;
+}
