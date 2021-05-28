e41f80aa793e5ac14b2b97fc64e99e5e6fde711f
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-05-28 10:01:45 +0530

    Create Find_wether_the_two_numbers_are_equal.c

---
+++ Find_wether_the_two_numbers_are_equal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int num1,num2;
+  printf("\n\nEnter the first number : ");
+  scanf("%d",&num1);
+  printf("\n\nEnter the second number : ");
+  scanf("%d",&num2);
+  if(num1==num2)
+  printf("\nThe numbers are equal.");
+  else
+  printf("\nThe numbers are not equal.");
+  return 0;
+}
