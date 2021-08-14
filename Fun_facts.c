39a89305ac3055a2886a258b738f49288f6c004b
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2021-08-14 19:26:48 +0530

    Create Fun_facts.c

---
+++ Fun_facts.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    
+    printf("*********************************");
+    printf("Hydrogen : 1");
+    printf("Helium : 2");
+    printf("Lithium : 3");
+    printf("Berilium : 4");
+    printf("Boron : 5");
+    printf("Carbon :6");
+    printf("Nitrogen : 7");
+    printf("Oxygen : 8");
+    printf("Flurine : 9");
+    printf("Neon : 10");
+    printf("Natrium : 11");
+    printf("Magnisium : 12");
+    printf("Aluminium :13");
+    printf("Silicon : 14");
+    printf("Prosperous : 15");
+    printf("Sodium : 16");
+    printf("Clorine : 17");
+    printf("Argon : 18");
+    printf("Potasium : 19");
+    printf("Cadmium : 20");
+    printf("Scandium : 21");
+    printf("Titanium : 22");
+    printf("Vanadium : 23");
+    printf("Cromium : 24");
+    printf("Manganese : 25");
+    printf("Iron : 26");
+    printf("Cobalt : 27");
+    printf("Nickel : 28");
+    printf("Copper : 29");
+    printf("Zinc : 30");
+    printf("Gallium : 31");
+    printf("Germanium : 32");
+    printf("Arsenic : 33");
+    printf("Seienium : 34");
+    printf("Bromine : 35");
+    printf("Krypton : 36");
+    printf("Rubidium : 37");
+    printf("*********************************");
+    return 0;
+}
