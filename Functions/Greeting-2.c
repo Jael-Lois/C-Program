7f251c8cb414dee70359276edfd0fa1b8edada7e
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2022-02-01 19:22:45 +0530

    Update Greeting-2.c

--- Functions/Greeting-2.c
+++ Functions/Greeting-2.c
@@ -3,12 +3,12 @@
 char a[100];
  void greeting()
 {
-  printf("Enter your name");
+  printf("Enter your name in the greeting function : ");
   scanf("%s",a);
 }
 int main()
 {
  greeting();
- printf("Good to see you %s",a);
+ printf("Good to see your name in the main function %s",a);
  return 0;
 }
