5a2a84a1aa11fe6e931c313a0e2d48ea65ff2aa0
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2022-02-03 14:37:00 +0530

    Update Equation.c

--- Functions/Equation.c
+++ Functions/Equation.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
 
-int a,b;
-
-void Equation()
+ int Equation(int a1,int a2,int c)
 {
   //x^2 + x(a+b) + (a*b)
-  printf("\nThe equation is x^2 + (%d)x + (%d) = 0",a+b,a*b);
+  if (c==1)
+  return (a1+a2);
+  else
+  return (a1*a2);
 }
 
 int main()
 {
+  int a,b,sum,product;
+
   printf("\nEnter the 1 numbers : ");
   scanf("%d",&a);
 
   printf("\nEnter the 2 numbers : ");
   scanf("%d",&b);
 
-  Equation();
-  
+  sum = Equation(a,b,1);
+  product = Equation(a,b,0);
+
+  printf("\n\nEquation : x^2 + (%d)x + %d",sum,product);
+
   return 0;
 }
