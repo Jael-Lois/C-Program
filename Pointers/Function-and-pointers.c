06bbc7ce6be2a6024bf0b5ec0780621d5c6cb624
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2022-02-10 19:35:56 +0530

    Create Function-and-pointers.c

---
+++ Pointers/Function-and-pointers.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+int Sumofasciivalue(char *a)
+{
+  int l,sum,i;
+
+  sum = 0;
+
+  l = strlen(a);
+
+  for(i=0;i<l;i++)
+    sum+=a[i];
+
+  return sum;
+}
+int main()
+{
+  int sum=0;
+
+  char name[20];
+
+  printf("Enter The name : ");
+  scanf("%s",name);
+
+  sum = Sumofasciivalue(name);
+
+  printf("The sum of the ascii values : %d",sum);
+
+  return 0;
+}
